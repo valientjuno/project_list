0b7753cde08535fbd99879906530cb09fe657382
valientjuno <jessedoake91@gmail.com>
2025-08-04 12:37:54 -0600

    update js

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-// NOTE: PLEASE READ EACH INSTRUCTION COMMENT CAREFULLY TO HELP YOU PROCESS THIS PROJECT
-// IF YOU ARE STUCK ASK AN INSTRUCTOR FOR HELP, ALWAYS WRITE THE CODE BELOW THE ASSIGNMENTS.
+// // NOTE: PLEASE READ EACH INSTRUCTION COMMENT CAREFULLY TO HELP YOU PROCESS THIS PROJECT
+// // IF YOU ARE STUCK ASK AN INSTRUCTOR FOR HELP, ALWAYS WRITE THE CODE BELOW THE ASSIGNMENTS.
 
-productArray = [];
+// productArray = [];
 
-let product = "We all Learn at a Pace";
+// let product = "We all Learn at a Pace";
 
-function learnAtWill(product) {
-  return `In class ${product}`;
+// function learnAtWill(product) {
+//   return `In class ${product}`;
+// }
+// console.log(learnAtWill(product));
+
+// let learn = "You will be a Success!";
+
+// function motivation(learn) {
+//   return `Don't give up persuing your Dream ${learn}`;
+// }
+// console.log(motivation(learn));
+let input = document.getElementById("product");
+let output = document.getElementById("output");
+let productArray = [];
+
+function addProduct() {
+  const newProductValue = input.value;
+  if (newProductValue === "") {
+    return;
+  }
+  productArray.push(newProductValue);
+  console.log(productArray);
+
+  // Clear previous output to prevent duplicates
+  output.innerHTML = "";
+
+  // Use forEach to iterate over the entire array and append each item
+  productArray.forEach((product) => {
+    const p = document.createElement("p");
+    p.textContent = product;
+    output.append(p);
+  });
+
+  input.value = "";
 }
-console.log(learnAtWill(product));
-
-let learn = "You will be a Success!";
-
-function motivation(learn) {
-  return `Don't give up persuing your Dream ${learn}`;
-}
-console.log(motivation(learn));
